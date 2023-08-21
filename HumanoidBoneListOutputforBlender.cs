69f1f81d5a820e3008f57ddc6a2954f201e952fd
D9speed <42368875+d9speed@users.noreply.github.com>
2023-08-21 21:29:46 +0900

    Output Unity humanoid Bone list

---
+++ HumanoidBoneListOutputforBlender.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Text;
+
+public class HumanoidBoneListOutputforBlender : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+                // Humanoidアバターのボーンの対応を出力
+        string objectName = this.gameObject.name;
+        StreamWriter sw = new StreamWriter(@"Assets\D9speed\"+objectName+"_BoneList.txt",false);
+        HumanDescription description = GetComponent<Animator>().avatar.humanDescription;
+        sw.Write("{");
+        for (int i = 0; i < description.human.Length; i++)
+        {
+            //JSONで書き出したいな
+            
+            string human_NameBone_Name = "'"+description.human[i].humanName+"'" + ":" + "'"+description.human[i].boneName+"'"+",";
+            //string indexAsString = i.ToString();
+            //sw.Write( "'" + indexAsString + "'" + ":");
+            sw.WriteLine(human_NameBone_Name);
+            
+        }
+        sw.Write("}");
+        sw.Close();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {}
+}
